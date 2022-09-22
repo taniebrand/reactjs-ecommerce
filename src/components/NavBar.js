a75c1b703e02cd1dbce8b9fbcd54e0e32bc95aa3
taniebrand <mat@taniebrand.com>
2022-09-21 21:04:01 -0300

    Add Navbar to project

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 const NavBar = () =>{
     return (
-        <div>New</div>
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Satelit Art</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
+            <NavDropdown title="Obras" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#foto">Foto</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#instalacion">
+                Instalacion
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        </div>
     )
 }
 export default NavBar;
